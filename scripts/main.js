4b13a4d68c01a700a78f9f9f555ceba1a809104d
Haoheng Tang <haoheng_1209@outlook.com>
2024-04-06 18:49:54 -0400

    update style, AMysteryforYou.html, Peashooter.html

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,27 @@
+/**************************
+      go to top button
+ **************************/
+// Get the button:
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+} else {
+        mybutton.style.display = "none";
+}
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+document.body.scrollTop = 0; // For Safari
+document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+} 
+
+
 /* ************************
         text opacity
 ************************** */
@@ -18,3 +42,50 @@ $(window).scroll(function () {
     var scrollBottom = $(document).height() - $(window).height() - $(window).scrollTop();
     nav_bottom.css("opacity", 1 - scrollBottom / 50);
  });
+
+
+
+/* ************************
+    scroll-to-play video
+************************** */
+//  const video = document.getElementById('videoID');
+//  video.pause();
+//  let playing = 0;
+//  let firsttime = true;
+//  window.addEventListener('scroll', function() {
+//         if (!firsttime){
+//    playing = 10;
+//         }
+//         firsttime = false;
+//  })
+
+//  setInterval(function(){
+//         playing--;
+//   if (playing>0){
+//         const rect = video.getBoundingClientRect();
+//         const isVisible = rect.top < window.innerHeight && rect.bottom >= 0;
+//         if (isVisible) {
+//           video.play();
+//         } else {
+//           video.pause();
+//         }
+//   }else{
+//         video.pause();
+//   }
+//  },100)
+
+const video= document.getElementById('videoID');
+let userScrolling = false;
+video.pause()
+
+window.addEventListener('scroll', function() {
+        userScrolling = true
+        const rect = video.getBoundingClientRect();
+        const isVisible = rect.top < window.innerHeight && rect.bottom >= 0;
+        if (isVisible) {
+          video.play();
+        } else {
+          video.pause();
+        }
+      });
+video.pause();
